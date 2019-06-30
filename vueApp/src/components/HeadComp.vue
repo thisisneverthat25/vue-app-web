467712728c2b1ea58be6582d043780c709664934
jkpark <>
2019-06-30 13:26:57 +0900

    #2 update

--- vueApp/src/components/HeadComp.vue
+++ vueApp/src/components/HeadComp.vue
@@ -5,6 +5,7 @@
         <router-link to="/">index</router-link>
         <router-link to="/1">1</router-link>
         <router-link to="/2">2</router-link>
+        <router-link to="/3">3</router-link>
       </div>
     </div>
 </template>
